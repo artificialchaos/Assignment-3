9d99155c2f4334c5aba51fb0ecb876fb70bb8b2f
Joe <c14518353@mydit.ie>
2016-04-20 00:51:54 +0100

    Initial APpplet setup

---
+++ life/lifesim/Main.java
@@ -0,0 +1,19 @@
+package lifesim;
+import processing.core.*;
+public class Main extends PApplet
+{
+
+	public static void main(String[] args) 
+	{
+		PApplet.main(new String[] { "--present", "Main" });
+	}
+	public void setup()
+	{
+	  size(1300, 800);
+	  background(0);
+	}
+	public void draw() 
+	{
+
+	}	
+}
---
+++ life/lifesim/bacteria.java
@@ -0,0 +1,16 @@
+package lifesim;
+import processing.core.PApplet;
+public class bacteria 
+{
+	void bateria(PApplet p)
+	{
+		
+	}
+	PApplet parent;
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
